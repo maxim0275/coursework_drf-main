16431e03f999a225ed1e9a2ab99bcf84c1a01ef2
maxim0275 <maxim0275@yandex.ru>
2026-03-21 08:51:49 +0500

    f1

--- config/urls.py
+++ config/urls.py
@@ -13,7 +13,7 @@ schema_view = get_schema_view(
         description="Документация для трекера полезных привычек",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"), )
+        license=openapi.License(name="BSD License"),)
     ,
     public=True,
     permission_classes=(permissions.AllowAny,), )
